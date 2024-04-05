711564eb1a9387076ecf852e4398fb1d99f10391
ssy <sufilal.syahdan@gmail.com>
2024-04-05 23:41:56 +0700

    feat: logical test

--- logical/index.js
+++ logical/index.js
@@ -3,6 +3,13 @@
  * @return {boolean}
  */
 
-const isPalindrome = (number) =>{
-    
-}
+const isPalindrome = (number) => {
+  let string = number.toString();
+  let string2 = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    string2 += string[i];
+  }
+
+  return string2 == string;
+};
