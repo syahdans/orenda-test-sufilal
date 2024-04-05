c0660830de0be826409a031fe8ca2ed915fab516
ssy <sufilal.syahdan@gmail.com>
2024-04-05 23:42:14 +0700

    Doc: add swagger

--- api/src/app.js
+++ api/src/app.js
@@ -3,16 +3,50 @@ const app = express();
 const port = 3000;
 const routes = require("./routes");
 const db = require("./database");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Sufilal syahdan",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
 
 const initApp = async () => {
   try {
     await db.authenticate();
     console.log("Connection has been established successfully.");
 
-    app
-      .use(bodyParser.urlencoded({ extended: false }))
-      .use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false })).use(bodyParser.json());
+
+    app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(specs, { explorer: true })
+    );
 
     app.use(express.json());
 
--- api/src/routes/customer.route.js
+++ api/src/routes/customer.route.js
@@ -1,18 +1,80 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/customer.controller');
-const validator = require('../validations')
-const validate = require('../utils/validate');
+const controller = require("../controllers/customer.controller");
+const validator = require("../validations");
+const validate = require("../utils/validate");
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - finished
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: The title of your book
+ *         author:
+ *           type: string
+ *           description: The book author
+ *         finished:
+ *           type: boolean
+ *           description: Whether you have finished reading the book
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the book was added
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ *         finished: false
+ *         createdAt: 2020-03-10T04:05:06.157Z
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ * /books:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Some server error
+ *
+ */
 
 // Define routes
 router.get("/", controller.getAll);
 
-router.get('/:id', controller.get);
+router.get("/:id", controller.get);
 
 router.post("/", validate(validator.customer.create), controller.create);
 
-router.put('/:id', validate(validator.customer.update), controller.update);
+router.put("/:id", validate(validator.customer.update), controller.update);
 
-router.delete('/:id', controller.remove);
+router.delete("/:id", controller.remove);
 
 module.exports = router;
